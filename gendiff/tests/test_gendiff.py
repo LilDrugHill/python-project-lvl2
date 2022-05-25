549a831ab4f6333c319bbbc28eb91eb20a77724a
LilDrugHill <smookigrow@icloud.com>
2022-05-25 11:44:07 +0300

    like lint said

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -5,4 +5,4 @@ def test_gendiff_simple():
     res = '{\n  - follow: False\n    host: hexlet.io\n  - proxy: \
 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
 
-    assert gendiff.generate_diff('gendiff/file1.json', 'gendiff/file2.json') == res
+    assert gendiff.generate_diff('gendiff/tests/fixtures/file1.json', 'gendiff/tests/fixtures/file2.json') == res
