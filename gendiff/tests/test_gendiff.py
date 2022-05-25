382829bab22a588ce70aaaea2831b9321bf13879
LilDrugHill <smookigrow@icloud.com>
2022-05-25 11:06:12 +0300

    add tests

---
+++ gendiff/tests/test_gendiff.py
@@ -0,0 +1,8 @@
+from gendiff import gendiff
+
+
+def test_gendiff_simple():
+    res = '{\n  - follow: False\n    host: hexlet.io\n  - proxy: \
+123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+
+    assert gendiff.generate_diff('gendiff/file1.json', 'gendiff/file2.json') == res
