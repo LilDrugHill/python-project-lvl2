1f92d408504ecc760b53f86c10e4da61991b271c
LilDrugHill <smookigrow@icloud.com>
2022-05-25 11:47:18 +0300

    some changes for linter

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -5,4 +5,6 @@ def test_gendiff_simple():
     res = '{\n  - follow: False\n    host: hexlet.io\n  - proxy: \
 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
 
-    assert gendiff.generate_diff('gendiff/tests/fixtures/file1.json', 'gendiff/tests/fixtures/file2.json') == res
+    file1 = 'gendiff/tests/fixtures/file1.json'
+    file2 = 'gendiff/tests/fixtures/file2.json'
+    assert gendiff.generate_diff(file1, file2) == res
