a5c8d1a15d583d6c975206128ee137b531539049
LilDrugHill <smookigrow@icloud.com>
2022-05-25 15:25:45 +0300

    convert in apart derectory

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-from gendiff.gendiff import generate_diff
+from gendiff.comparator import generate_diff
+from gendiff.converstors.json_conv import convert_json
+from gendiff.converstors.yaml_conv import convert_yaml
 
 
 def main():
@@ -14,7 +16,15 @@ def main():
 
     args = parser.parse_args()
 
-    print(generate_diff(args.first_file, args.second_file))
+    if args.format == 'json':
+        file1 = convert_json(args.first_file)
+        file2 = convert_json(args.second_file)
+
+    if args.format == 'yaml':
+        file1 = convert_yaml(args.first_file)
+        file2 = convert_yaml(args.second_file)
+
+    print(generate_diff(file1, file2))
 
 
 if __name__ == '__main__':
