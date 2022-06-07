362f772a194ce3d8541712e2fd55124ddae42624
LilDrugHill <smookigrow@icloud.com>
2022-06-07 17:30:39 +0300

    Added ability to compare deep structures

--- gendiff/comparator.py
+++ gendiff/comparator.py
@@ -1,25 +1,55 @@
 def generate_diff(file1, file2):
-    output_str = ''
-    output_str += '{\n'
 
-    sorted_keys = sorted(set(file1) | set(file2))
+    def walk(node1, node2, key=0):
 
-    for key in sorted_keys:
-        file1_key_value = file1.get(key)
-        file2_key_value = file2.get(key)
+        if not isinstance(node1, dict) or not isinstance(node2, dict):
+            return pair_gen(node1, node2, key)
 
-        output_str += build_diff_line(key, file1_key_value, file2_key_value)
+        if isinstance(node1, dict) and isinstance(node2, dict):
+            sorted_children = sorted(set(node1) | set(node2))
 
-    output_str += '}'
-    return output_str
+        new_children = list(map(
+            lambda child: walk(node1.get(child, '&'), node2.get(child, '&'), child),
+            sorted_children))
+
+        return new_tree_gen(key, new_children)
+
+    return walk(file1, file2)
 
 
-def build_diff_line(key, value1, value2):
+def pair_gen(node1, node2, key):
+
+    value1 = node1
+    value2 = node2
+    if value1 == '&':
+        return (f'+{key}', value2)
+    if value2 == '&':
+        return (f'-{key}', value1)
     if value1 == value2:
-        return f'    {key}: {value1}\n'
-    if value2 is None:
-        return f'  - {key}: {value1}\n'
-    if value1 is None:
-        return f'  + {key}: {value2}\n'
-    if value1 != value2:
-        return f'  - {key}: {value1}\n  + {key}: {value2}\n'
+        return (f'{key}', value1)
+    if value1 != value2 and not isinstance(value1, dict) or not isinstance(value2, dict):
+        return (
+            (f'-{key}', value1),
+            (f'+{key}', value2),
+        )
+
+
+def new_tree_gen(key, new_children):
+    new_sorted_children = []
+    new_values = []
+    for child in new_children:
+        if isinstance(child, dict):
+            new_children[new_children.index(child)] = (key, child)
+        elif isinstance(child[0], tuple):
+            new_sorted_children.append(child[0][0])
+            new_sorted_children.append(child[1][0])
+            new_values.append(child[0][1])
+            new_values.append(child[1][1])
+        elif isinstance(child, tuple):
+            new_sorted_children.append(child[0])
+            new_values.append(child[1])
+
+    new_tree = {key: new_children for key, new_children in zip(new_sorted_children, new_values)}
+    if key != 0:
+        new_tree = (key, new_tree)
+    return new_tree
