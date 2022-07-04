c3ebb3bbcfbbfede59c13b6be8e8b0f103706bf1
LilDrugHill <smookigrow@icloud.com>
2022-07-04 16:03:34 +0300

    7th patch

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,3 +1,10 @@
+import itertools
+
+
+REPLACER = ' '
+SPACE_COUNT = 4
+
+
 def change_structure(value):
 
     def walk(node, key=0):
@@ -15,7 +22,7 @@ def change_structure(value):
             new_children.update(child)
 
         if key != 0:
-            return {key: new_children}
+            return {f'{key}': new_children}
 
         return new_children
 
@@ -35,65 +42,31 @@ def build_leaf(node, key):
     return {f'{key}': value1}
 
 
-REPLACER = ' '
-SPACE_COUNT = 4
-
-
 def stringify(value):
 
-    def walk(node, depth, path):
+    def walk(current_value, depth):
+        if not isinstance(current_value, dict):
+            return str(current_value)
 
-        if isinstance(node, tuple) and not isinstance(node[1], dict):
-            return REPLACER * SPACE_COUNT * depth + f'{node[0]}: {node[1]}'
-
-        path = ''
-
-        if isinstance(node, dict):
-            children = node.items()
-
-        else:
-            path += REPLACER * SPACE_COUNT * depth + node[0] + ': '
-            children = node[1].items()
-
-        string_node = str(list(map(lambda child: walk(child, depth + 1, path), children)))
-
-        return path + string_node + f'{depth}'
+        deep_indent_size = depth + SPACE_COUNT
+        deep_indent = REPLACER * (deep_indent_size)
+        current_indent = REPLACER * depth
+        lines = []
+        for key, val in current_value.items():
+            lines.append(f'{deep_indent}{key}: {walk(val, deep_indent_size)}')
+        result = itertools.chain("{", lines, [current_indent + "}"])
+        return '\n'.join(result)
 
     if isinstance(value, dict):
-        return visual(walk(change_structure(value), 0, ''))
+        return replace_(walk(change_structure(value), 0))
 
     return str(value)
 
 
-def visual(value):
-    index = 0
-    while index < len(value):
-
-        if value[index] == ']':
-            seporator_position = value[index + 1]
-            value = value.replace(
-                f']{seporator_position}',
-                (f'\n{REPLACER * SPACE_COUNT * int(seporator_position)}' + '}'))
-            index = 0
-            continue
-
-        index += 1
-
-    return to_str(value)
-
-
-def to_str(data):
-
-    completed_str = (data
-                     .replace('True', 'true')
-                     .replace('None', 'null')
-                     .replace('False', 'false')
-                     .replace('[', '{\n')
-                     .replace(', ', '\n')
-                     .replace("'", '')
-                     .replace('"', '')
-                     .replace('\\', '')
-                     .replace('  +', '+ ')
-                     .replace('  -', '- '))
-
-    return completed_str
+def replace_(value):
+    new_value = (value.replace('False', 'false')
+                      .replace('True', 'true')
+                      .replace('None', 'null')
+                      .replace('  +', '+ ')
+                      .replace('  -', '- '))
+    return new_value
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,119 +1,24 @@
-import json
 import pytest
 from gendiff.gendiff import generate_diff
-from gendiff.comparator import build_diff_tree, pair_gen
-from gendiff.formatters.json_format import json_format
-from gendiff.formatters.plain import plain
-from gendiff.formatters.stylish import stringify
-from gendiff.input_parser import get_data
 
 
-expected_res_simple = {'follow': {'action': 'remove', 'node1': False},
-                       'host': {'action': 'nested', 'node1': 'hexlet.io'},
-                       'proxy': {'action': 'remove', 'node1': '123.234.53.22'},
-                       'timeout': {'action': 'changed', 'node1': 50, 'node2': 20},
-                       'verbose': {'action': 'add', 'node2': True}}
-
-expected_res_deep = {'common': {'follow': {'action': 'add', 'node2': False},
-                                'setting1': {'action': 'nested', 'node1': 'Value 1'},
-                                'setting2': {'action': 'remove', 'node1': 200},
-                                'setting3': {'action': 'changed', 'node1': True, 'node2': None},
-                                'setting4': {'action': 'add', 'node2': 'blah blah'},
-                                'setting5': {'action': 'add', 'node2': {'key5': 'value5'}},
-                                'setting6': {'doge': {'wow': {'action': 'changed',
-                                                              'node1': '',
-                                                              'node2': 'so much'}},
-                                             'key': {'action': 'nested', 'node1': 'value'},
-                                             'ops': {'action': 'add', 'node2': 'vops'}}},
-                     'group1': {'baz': {'action': 'changed', 'node1': 'bas', 'node2': 'bars'},
-                                'foo': {'action': 'nested', 'node1': 'bar'},
-                                'nest': {'action': 'changed',
-                                         'node1': {'key': 'value'},
-                                         'node2': 'str'}},
-                     'group2': {'action': 'remove', 'node1': {'abc': 12345, 'deep': {'id': 45}}},
-                     'group3': {'action': 'add',
-                                'node2': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}
+FIXTURE_PATH = 'tests/fixtures/deep_files/'
+OUTPUT_PATH = 'tests/fixtures/outputs/'
 
 
-@pytest.mark.parametrize("file_path1,file_path2,expected",
-                         [('tests/fixtures/deep_files/file1.json',
-                           'tests/fixtures/deep_files/file2.json',
-                           expected_res_deep),
-                          ('tests/fixtures/simple_files/file1.json',
-                           'tests/fixtures/simple_files/file2.json',
-                           expected_res_simple)])
-def test_build_diff_tree(file_path1, file_path2, expected):
-    file1, file2 = get_data(file_path1), get_data(file_path2)
-    assert build_diff_tree(file1, file2) == expected
-
-
-@pytest.mark.parametrize("value1,value2,key,expected",
-                         [(False, '&', 'follow', {'follow': {'action': 'remove', 'node1': False}}),
-                          ('&', True, 'verbose', {'verbose': {'action': 'add', 'node2': True}}),
-                          (50, 20, 'timeout', {'timeout': {'action': 'changed',
-                                                           'node1': 50,
-                                                           'node2': 20}}),
-                          ('hexlet.io', 'hexlet.io', 'host', {'host': {'action': 'nested',
-                                                                       'node1': 'hexlet.io'}})])
-def test_pair_gen(value1, value2, key, expected):
-    assert pair_gen(value1, value2, key) == expected
-
-
-@pytest.mark.parametrize("formater,input1,input2,output",
-                         [(stringify,
-                           'tests/fixtures/simple_files/file1.yaml',
-                           'tests/fixtures/simple_files/file2.yaml',
-                           'tests/fixtures/outputs/stylish_simple.txt'),
-                          (stringify,
-                           'tests/fixtures/deep_files/file1.yml',
-                           'tests/fixtures/deep_files/file2.yml',
-                           'tests/fixtures/outputs/stylish_deep.txt'),
-                          (plain,
-                           'tests/fixtures/simple_files/file1.json',
-                           'tests/fixtures/simple_files/file2.json',
-                           'tests/fixtures/outputs/plain_simple.txt'),
-                          (plain,
-                           'tests/fixtures/deep_files/file1.json',
-                           'tests/fixtures/deep_files/file2.json',
-                           'tests/fixtures/outputs/plain_deep.txt'),
-                          (json_format,
-                           'tests/fixtures/deep_files/file1.json',
-                           'tests/fixtures/deep_files/file2.json',
-                           'tests/fixtures/outputs/deep_file.json')])
-def test_formatters(formater, input1, input2, output):
-    if formater is json_format:
-        with open(output, encoding='utf-8') as read_file:
-            read_file = json.load(read_file)
-            output = json.dumps(read_file, indent=4)
-    else:
-        with open(output, encoding='utf-8') as read_file:
-            output = read_file.read()
-
-    data1, data2 = get_data(input1), get_data(input2)
-
-    assert formater(build_diff_tree(data1, data2)) == output
-
-
-#  Вы имели ввиду единственный тест как ниже?
-@pytest.mark.parametrize("input1,input2,output_format,output",
-                         [('tests/fixtures/deep_files/file1.yml',
-                           'tests/fixtures/deep_files/file2.yml',
+@pytest.mark.parametrize("file1,file2,output_format,output_file",
+                         [('file1.yml',
+                           'file2.yml',
                            None,
-                           'tests/fixtures/outputs/stylish_deep.txt'),
-                          ('tests/fixtures/deep_files/file1.json',
-                           'tests/fixtures/deep_files/file2.json',
+                           'stylish_deep.txt'),
+                          ('file1.json',
+                           'file2.json',
                            'plain',
-                           'tests/fixtures/outputs/plain_deep.txt'),
-                          ('tests/fixtures/deep_files/file1.json',
-                           'tests/fixtures/deep_files/file2.json',
-                           'json',
-                           'tests/fixtures/outputs/deep_file.json')])
-def test_gendiff(input1, input2, output_format, output):
-    if output_format == 'json':
-        with open(output, encoding='utf-8') as read_file:
-            read_file = json.load(read_file)
-            output = json.dumps(read_file, indent=4)
-    else:
-        with open(output, encoding='utf-8') as read_file:
-            output = read_file.read()
-    assert generate_diff(input1, input2, output_format) == output
+                           'plain_deep.txt')])
+def test_gendiff(file1, file2, output_format, output_file):
+    file_path1 = FIXTURE_PATH + file1
+    file_path2 = FIXTURE_PATH + file2
+    output_path = OUTPUT_PATH + output_file
+    with open(output_path, encoding='utf-8') as read_file:
+        output = read_file.read()
+    assert generate_diff(file_path1, file_path2, output_format) == output
